70b8d537566efa2aad5f1623086713a2b9a67d6b
undo20 <ducvandang@outlook.com>
2015-07-12 00:54:38 +0200

    php

---
+++ git_upload.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user8
+ * Date: 12.07.2015
+ * Time: 00:18
+ */
+echo "uploaded to github";
---
+++ hello_world.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user8
+ * Date: 11.07.2015
+ * Time: 23:50
+ */
+echo "Hello world!";
+echo "test";
